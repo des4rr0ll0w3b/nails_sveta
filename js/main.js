7f43431ffc1ca051b16b9a4c1fc6010df4443e5a
des4rr0ll0w3b <des4rr0ll0w3b@gmail.com>
2025-08-27 18:16:16 +0200

    Nueva actualización: margenes estabilizadas6

--- js/main.js
+++ js/main.js
@@ -127,10 +127,14 @@ async function cargarHeaderFooterYInicializar() {
         .then(data => {
           document.getElementById("header").innerHTML = data;
         }),
+
+        // FOOTER//
       fetch("footer.html")
-        .then(res => res.text())
-        .then(data => {
-          document.getElementById("footer").innerHTML = data;
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+
+          
         }),
     ]);
 
